7d4f98f6a15abdbb78c4c634c3f1c947b603440d
LiubovSukhoivan <86472972+LiubovSukhoivan@users.noreply.github.com>
2021-06-25 14:14:55 +0300

    Delete View.java

--- View.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class View {
-    public void printStudentInfo(String studentName, String lesson){
-        System.out.println("Student: ");
-        System.out.println("Name: "+studentName);
-        System.out.println("Lesson "+lesson);
-    }
-}
